765fc6292f510133bf347b162bfb1eba952975ad
Irina <anatskayairina@gmail.com>
2022-12-12 19:57:53 +0100

    program added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string [] FirstArray = new string [5] {"Hello", "World", ":-)", "-5", "24"};
+string [] SecondArray = new string [FirstArray.Length];
+SecondArrayResult (FirstArray, SecondArray);
+PrintArray (SecondArray);
+
+
+void SecondArrayResult (string [] FirstArray, string [] SecondArray)
+{
+    int count = 0;
+    for ( int i = 0; i < FirstArray.Length; i ++)
+    {
+        if (FirstArray [i].Length <= 3)
+        {
+            SecondArray [count] = FirstArray [i];
+            count ++;
+        }
+    }
+}
+
+void PrintArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i ++)
+    {
+        Console.Write ($"{array [i]} ");
+    }
+    Console.WriteLine();
+}
